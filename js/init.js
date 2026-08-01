36127fe051068f30073c4b720fa904f29ba28ec3
1054009064 <44148148+1054009064@users.noreply.github.com>
2026-08-01 19:25:56 +0800

    2.7.2

--- js/init.js
+++ js/init.js
@@ -172,6 +172,7 @@ G.OptionLists = {
 
     iframeFFmpeg: false,    // 潜入在线ffmpeg
     contextMenus: false,    // 右键菜单
+    reverse: false, // 倒叙排序
 };
 
 // 本地储存的配置
@@ -183,6 +184,8 @@ G.LocalVar = {
     // 预览页面
     previewShowTitle: false, // 是否显示标题
     previewDeleteDuplicateFilenames: false, // 是否删除重复文件名
+
+    M3u8HideDownloadedSegments: false, // m3u8下载完成后隐藏已下载的切片
 };
 
 // 102版本以上 非Firefox 开启更多功能
--- js/polyfill.js
+++ js/polyfill.js
@@ -1,23 +1,3 @@
-// Service Worker 5分钟后会强制终止扩展
-// https://bugs.chromium.org/p/chromium/issues/detail?id=1271154
-// https://stackoverflow.com/questions/66618136/persistent-service-worker-in-chrome-extension/70003493#70003493
-chrome.webNavigation.onBeforeNavigate.addListener(function () { return; });
-chrome.webNavigation.onHistoryStateUpdated.addListener(function () { return; });
-chrome.runtime.onConnect.addListener(function (Port) {
-    if (chrome.runtime.lastError || Port.name !== "HeartBeat") return;
-    Port.postMessage("HeartBeat");
-    Port.onMessage.addListener(function (message, Port) { return; });
-    const interval = setInterval(function () {
-        clearInterval(interval);
-        Port.disconnect();
-    }, 250000);
-    Port.onDisconnect.addListener(function () {
-        interval && clearInterval(interval);
-        if (chrome.runtime.lastError) { return; }
-    });
-});
-setInterval(chrome.runtime.getPlatformInfo, 25 * 1000);
-
 // 低版本chrome manifest v3协议 会有 getMessage 函数不存在的bug
 if (chrome.i18n.getMessage === undefined) {
     chrome.i18n.getMessage = (key) => key;
@@ -59,3 +39,31 @@ const i18n = new Proxy(chrome.i18n.getMessage, {
         return chrome.i18n.getMessage(key);
     }
 });
+
+// 手机浏览器不支持右键菜单
+if (!chrome.contextMenus) {
+    chrome.contextMenus = {
+        create: function () { },
+        remove: function () { },
+        removeAll: function () { },
+        onClicked: { addListener: function () { } },
+    }
+}
+
+// 手机浏览器没有windows API 只能使用tabs API
+if (!chrome.windows) {
+    chrome.windows = {
+        onFocusChanged: { addListener: function () { } },
+        create: function (obj, callback) {
+            chrome.tabs.create({ url: obj.url }, function (tab) {
+                callback && callback(tab);
+            });
+        },
+    }
+}
+// 手机浏览器木有快捷键
+if (!chrome.commands) {
+    chrome.commands = {
+        onCommand: { addListener: function () { } }
+    }
+}
